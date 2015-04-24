d8bb3f18835c1d16c718ba6a241f78a39320e555
wutong <asivy123@gmail.com>
2015-04-24 11:30:59 +0800

    去掉无用注释

--- src/main/java/wu/tong/precise/wakeup/core/LiveCache.java
+++ src/main/java/wu/tong/precise/wakeup/core/LiveCache.java
@@ -2,7 +2,6 @@ package wu.tong.precise.wakeup.core;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
 import java.util.TreeMap;
